8c078798c8a98cae1903d5eccfa30b22bded5e7c
David Scandurra <david.scandurra@check24.de>
2023-04-03 11:50:03 +0200

    publish jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     application
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "io.github.splotycode"
+version = "git rev-parse --short=8 HEAD".runCommand(workingDir = rootDir)
+val main = "MainKt";
+project.setProperty("mainClassName", main)
 
 repositories {
     mavenCentral()
@@ -22,5 +26,43 @@ tasks.withType<KotlinCompile> {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set(main)
+}
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/SplotyCode/WannGym")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+            artifact ("build/libs/WannGym-$version-all.jar") {
+                classifier = "fat"
+                extension  = "jar"
+            }
+        }
+    }
+}
+
+fun String.runCommand(workingDir: File = file("./")): String {
+    val parts = this.split("\\s".toRegex())
+    val proc = ProcessBuilder(*parts.toTypedArray())
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    proc.waitFor(1, TimeUnit.MINUTES)
+    return proc.inputStream.bufferedReader().readText().trim()
 }
