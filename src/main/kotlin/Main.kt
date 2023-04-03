3ec2d655c8ff608cce3b21755dd7d23014f19312
David Scandurra <david.scandurra@check24.de>
2023-04-03 13:45:37 +0200

    add error handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -18,12 +18,28 @@ fun main() {
     store.connect("database.db")
 
     studios.forEach { (name, identifier) ->
-        val response = JSONObject(URL("$API$identifier").readText()).getJSONObject("data")
-        val current = response.getInt("check_ins")
-        val allowed = response.getInt("allowed_people")
-        val usage = current / allowed.toDouble();
-        val percentage = NumberFormat.getPercentInstance().format(usage)
-        println("$name: $current/$allowed $percentage")
-        store.storeInfo(name, current, allowed, usage)
+        val rawResponse = rawResponse(identifier)
+        try {
+            val response = JSONObject(rawResponse).getJSONObject("data")
+            val current = response.getInt("check_ins")
+            val allowed = response.getInt("allowed_people")
+            val usage = current / allowed.toDouble();
+            val percentage = NumberFormat.getPercentInstance().format(usage)
+            println("$name: $current/$allowed $percentage")
+            store.storeInfo(name, current, allowed, usage)
+            Thread.sleep(500)
+        } catch (error: Exception) {
+            println("Failed to handle response: $rawResponse for $name")
+            error.printStackTrace()
+        }
+    }
+}
+
+fun rawResponse(gym: String): String {
+    return try {
+        URL("$API$gym").readText()
+    } catch (error: Throwable) {
+        error.printStackTrace()
+        error.message ?: "Unknown error"
     }
 }
